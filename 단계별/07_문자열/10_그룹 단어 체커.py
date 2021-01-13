9d7c8e8b423246909906aebcd7653fee1e88149f
hyein99 <hyeinnn999@gmail.com>
2021-01-13 15:56:39 +0900

    0113test

---
+++ 단계별/07_문자열/10_그룹 단어 체커.py
@@ -0,0 +1,17 @@
+num = int(input())
+
+cnt = num
+for _ in range(num):
+    word = list(input())
+
+    rep = word[0]
+    alpha = [rep]
+    for w in word:
+        if w != rep:
+            if w in alpha:
+                cnt -= 1
+                break
+            rep = w
+            alpha.append(rep)
+
+print(cnt)
