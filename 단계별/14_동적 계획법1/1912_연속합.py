956030f5906b8bb78f4e0e7adeaafe03ddc8abd8
hyein99 <hyeinnn999@gmail.com>
2021-02-18 19:45:05 +0900

    0218_1

---
+++ 단계별/14_동적 계획법1/1912_연속합.py
@@ -0,0 +1,25 @@
+import sys
+
+# 방법1: 메모이제이션
+def sub_sum_1(nums):
+    for i in range(1, len(nums)):
+        nums[i] += nums[i-1] if nums[i-1] > 0 else 0
+
+    return max(nums)
+
+# 방법2: 카데인 알고리즘
+def sub_sum_2(nums):
+    maxsum = -sys.maxsize
+    cursum = 0
+    for num in nums:
+        cursum = max(num, cursum+num)
+        maxsum = max(maxsum, cursum)
+
+    return maxsum
+
+# 입력
+n = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+
+print(sub_sum_1(nums))
+print(sub_sum_2(nums))
---
+++ 단계별/14_동적 계획법1/2579_계단 오르기.py
@@ -0,0 +1,28 @@
+import sys
+from collections import defaultdict
+
+# 타뷸레이션(상향식)
+dp = defaultdict()
+def get_stairs_sum(n):
+    if n==1:   # 1, 2는 직접 설정
+        return stairs[0]
+    elif n==2:
+        return stairs[0]+stairs[1]
+    else:
+        dp[1] = (stairs[0], stairs[0])
+        dp[2] = (stairs[1], stairs[0]+stairs[1])
+        # i번째 계단에 올라가는 방법은 1) i-2번째 계단에서 2계단 2) i-1번째 계단에서 1계단
+        # dp[i]에 (1계단 연속인 경우, 2계단 연속인 경우)를 저장하여 3계단 연속 방지
+        # 1) i-2에서 올라올 경우 3계단 연속을 생각하지 않아도 됨
+        # 2) i-1에서 올라올 경우 1계단 연속인 경우만 가져올 수 있음
+        for i in range(3, n+1):
+            dp[i] = (max(dp[i-2])+stairs[i-1], dp[i-1][0]+stairs[i-1])
+    return max(dp[n])
+
+
+T = int(sys.stdin.readline())
+stairs = []
+for _ in range(T):
+    stairs.append(int(sys.stdin.readline()))
+
+print(get_stairs_sum(len(stairs)))
