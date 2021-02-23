01a4a353bd418fe69f8d728e6f8f0ac91a4b011c
hyein99 <hyeinnn999@gmail.com>
2021-02-23 13:51:49 +0900

    0223

--- 단계별/14_동적 계획법1/9251_LCS.py
+++ 단계별/14_동적 계획법1/9251_LCS.py
@@ -4,19 +4,20 @@ import sys
 
 def LCS(str1, str2):
     result = 0
-    dp = [(0,0) for _ in range(len(str1))]
+    dp = [[0,0] for _ in range(len(str1))]
     # dp[i]: (str1[i]를 마지막으로 하는 문자열 중 str2에 있고 가장 긴 문자열의 길이,
     #         그 문자열의 str2 위치+1)
 
     for i in range(len(str1)):
         for j in range(i,-1,-1):
             x = str2[dp[j][1]:].find(str1[i])
-            if x != -1 and dp[j][0]+1 > dp[i][0]:
+            if x != -1 and (                  # str[i]가 str2에 존재
+                    dp[j][0]+1 > dp[i][0] or  # 가장 긴 문자열
+                    (dp[j][0]+1 == dp[i][0] and dp[i][1] > dp[j][1]+x+1)):
+                     # 길이가 같고 끝난 str2위치가 더 짧은 경우
                 dp[i] = (dp[j][0]+1, dp[j][1]+x+1)
             if dp[i][0] > result:
                 result = dp[i][0]
-    #         print(dp)
-    # print()
     print(dp)
     return result
 
@@ -25,6 +26,9 @@ str1 = sys.stdin.readline().rstrip()
 str2 = sys.stdin.readline().rstrip()
 
 print(LCS(str1, str2))
+
+# abcdef
+# cdefabc
 
 # SKDFHWEODJKSFSDFJK
 # WKJSDHFOWEFKJDVKSDF
@@ -37,27 +41,3 @@ print(LCS(str1, str2))
 # LKSDJFLKJWPOJSDLKJFSDF
 # OISJDKFJLKEJFSLKJDIFJSLDK
 # 정답: 13, 이 코드: 11
-
-
-# A
-# - A: A > (1,1) >> dp[0]
-# C
-# - AC: AC > (2,3) >> dp[1]
-# - C: C > (1,0)
-# A
-# - AA: AA > (2,4)
-# - ACA: ACA > (3,4) >> dp[2]
-# - CA: CA > ()
-# Y
-# - AY: x > (0,0) >> dp[3]
-# - ACY: x
-# - AAY: x
-# - ACAY: x
-# K
-# - AK: AK > (2,5)
-# - ACK: ACK > (3,5)
-# - AAK: AAK > (3,5)
-# - ACAK: ACAK > (4,5) >> dp[4]
-# P
-# - AP: AP > (2,2)
-# - ACP:
