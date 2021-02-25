06c1ea10858d65d8b63c81a305bac74633ba73be
hyein99 <hyeinnn999@gmail.com>
2021-02-25 09:42:11 +0900

    0225

--- 단계별/15_그리디 알고리즘/1931_회의실 배정.py
+++ 단계별/15_그리디 알고리즘/1931_회의실 배정.py
@@ -4,4 +4,14 @@ N = int(sys.stdin.readline())
 conf = []
 for _ in range(N):
     conf.append(list(map(int, sys.stdin.readline().split())))
-print(sorted(conf))
+conf.sort()
+
+cnt, end = 0, 0
+for i in range(len(conf)):
+    print(i)
+    if conf[i][0] >= end:
+        cnt += 1
+        end = conf[i][1]
+    if conf[i][1] < end:
+        end = conf[i][1]
+print(cnt)
