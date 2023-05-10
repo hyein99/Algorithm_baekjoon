d55c318d2e4b5e6853838a5954c7ef213d30a801
hyein99 <hyeinnn999@gmail.com>
2023-05-11 01:09:34 +0900

    algo_0511

--- 분류별/백트래킹/9663_N-Queen.py
+++ 분류별/백트래킹/9663_N-Queen.py
@@ -1,6 +1,29 @@
-def nqueen(start):
-    pass
+def is_available(r):
+    for i in range(r):
+        if board[i] == board[r] or abs(r-i) == abs(board[r]-board[i]):
+            return False
+    return True
 
 
-# input
+def DFS(cur_row):
+    global cnt
+
+    if cur_row == N:
+        cnt += 1
+        return
+
+    for cur_col in range(N):
+        board[cur_row] = cur_col  # cur_row의 cur_col에 queen
+        if is_available(cur_row):
+            visited[cur_col] = True
+            DFS(cur_row + 1)
+            visited[cur_col] = False
+
+
 N = int(input())
+board = [0] * N
+visited = [False] * N
+cnt = 0
+
+DFS(0)
+print(cnt)
